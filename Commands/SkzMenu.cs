c9205a66b1e134a8d01db4f2458341ecd15c9c67
Onurcan ertekin <ertekinonurcan@gmail.com>
2023-12-31 21:33:44 +0300

    skz

---
+++ Commands/SkzMenu.cs
@@ -0,0 +1,87 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Menu;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace JailbreakExtras;
+
+public partial class JailbreakExtras
+{
+    private Dictionary<string, Vector> skzCoordinates = new()
+    {
+        {"Hücre",new Vector(0,0,0) },
+        {"KZ",new Vector(0,0,0) },
+    };
+
+    #region SKZ
+
+    [ConsoleCommand("skz")]
+    [CommandHelper(0, "<saniye>")]
+    public void SkzMenu(CCSPlayerController? player, CommandInfo info)
+    {
+        if (ValidateCallerPlayer(player) == false)
+        {
+            return;
+        }
+
+        if (info.ArgCount != 2)
+        {
+            return;
+        }
+        var target = info.GetArg(1);
+        if (int.TryParse(target, out var value))
+        {
+            if (value > 120)
+            {
+                player.PrintToChat("Max 120 sn girebilirsin");
+                return;
+            }
+        }
+        var skzMenu = new ChatMenu("SKZ Menü");
+        foreach (var k in skzCoordinates.ToList())
+        {
+            skzMenu.AddMenuOption(k.Key, (p, t) =>
+            {
+                Server.PrintToChatAll($" {ChatColors.LightRed}[ZMTR] {ChatColors.White}Mahkûmlar {ChatColors.Blue}{k.Key} {ChatColors.White} ışınlanıyor");
+                BasicCountdown.CommandStartTextCountDown(this, $" {ChatColors.LightRed}[ZMTR] {ChatColors.White} Mahkûmların donmasina {ChatColors.Blue}{value} {ChatColors.White} saniye");
+
+                GetPlayers(CsTeam.Terrorist)
+                    .Where(x => x.PawnIsAlive == true)
+                    .ToList()
+                    .ForEach(x =>
+                    {
+                        x.PlayerPawn.Value.Teleport(k.Value, new QAngle(0f, 0f, 0f), new Vector(0f, 0f, 0f));
+                    });
+
+                _ = AddTimer(value, () =>
+                {
+                    GetPlayers()
+                    .Where(x => x != null
+                         && x.PlayerPawn.IsValid
+                         && x.PawnIsAlive
+                         && x.IsValid
+                         && x?.PlayerPawn?.Value != null
+                         && GetTeam(x) == CsTeam.Terrorist)
+                    .ToList()
+                    .ForEach(x =>
+                    {
+                        SetColour(x, Config.BuryColor);
+
+                        x.PlayerPawn.Value!.MoveType = MoveType_t.MOVETYPE_OBSOLETE;
+                        Vector currentPosition = x.Pawn.Value!.CBodyComponent?.SceneNode?.AbsOrigin ?? new Vector(0, 0, 0);
+                        Vector currentSpeed = new Vector(0, 0, 0);
+                        QAngle currentRotation = x.PlayerPawn.Value.EyeAngles ?? new QAngle(0, 0, 0);
+                        x.PlayerPawn.Value.Teleport(currentPosition, currentRotation, currentSpeed);
+                    });
+                    FreezeOrUnfreezeSound();
+                    Server.PrintToChatAll($" {ChatColors.LightRed}[ZMTR] {ChatColors.Green}{player.PlayerName}{ChatColors.White} adlı admin, {ChatColors.Green}mahkûmları {ChatColors.Blue}dondurdu{ChatColors.White}.");
+                });
+            });
+        }
+        ChatMenus.OpenMenu(player, skzMenu);
+    }
+
+    #endregion SKZ
+}
