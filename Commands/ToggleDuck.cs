f356c042639ab2411e4842ad70ec810dd5530125
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-01-03 16:14:51 +0300

    toggle duck cmd

---
+++ Commands/ToggleDuck.cs
@@ -0,0 +1,22 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+
+namespace JailbreakExtras;
+
+public partial class JailbreakExtras
+{
+    #region ToggleDuck
+
+    [ConsoleCommand("toggleDuck")]
+    public void ToggleDuck(CCSPlayerController? player, CommandInfo info)
+    {
+        if (ValidateCallerPlayer(player, false))
+        {
+            player.ExecuteClientCommand("alias test \"bind k test2; +duck\";alias test2 \"bind k test; -duck\";bind k test");
+            player.PrintToChat($" {CC.LR}[ZMTR] {CC.G}K {CC.W} tuşu ile eğilip kalkabilirsin.");
+        }
+    }
+
+    #endregion ToggleDuck
+}
