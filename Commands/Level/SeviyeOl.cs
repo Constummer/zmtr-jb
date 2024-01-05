e101da3544735c5702302a646b9853e4083a6c05
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-01-05 20:29:45 +0300

    seviyeol fix

--- Commands/Level/SeviyeOl.cs
+++ Commands/Level/SeviyeOl.cs
@@ -20,16 +20,16 @@ public partial class JailbreakExtras
         {
             InsertAndGetPlayerLevelData(player.SteamID);
             player.PrintToCenter($" {CC.LR}[ZMTR] {CC.G} Seviye sistemine Hoşgeldin.");
-            player.PrintToCenter($" {CC.LR}[ZMTR] {CC.B} !seviye{CC.W}, {CC.B}!seviyem{CC.W}, {CC.B}!tp {CC.W} yazarak TP puanini öğrenebilirsin");
             player.PrintToCenter($" {CC.LR}[ZMTR] {CC.G} Sunucuda bulunduğun süre boyunca dakikada {CC.R}1{CC.G} TP Kazanacaksin");
-            player.PrintToCenter($" {CC.LR}[ZMTR] {CC.G} Seviye sisteminden ayrılmak istersen {CC.R}!seviyeayril {CC.G} ile ayrılabilirsin");
-            player.PrintToCenter($" {CC.LR}[ZMTR] {CC.G} Seviye sisteminin ayrıcalıkları hakkında bilgi almak için discorda katılmalısın ({CC.B}!dc{CC.G})");
-            player.PrintToCenter($" {CC.LR}[ZMTR] {CC.Gr} TEKRAR HOŞ GELDİN");
         }
         else
         {
             player.PrintToCenter($" {CC.LR}[ZMTR] {CC.G} Halihazirda slotsun.");
         }
+        player.PrintToCenter($" {CC.LR}[ZMTR] {CC.B} !seviye{CC.W}, {CC.B}!seviyem{CC.W}, {CC.B}!tp {CC.W} yazarak TP puanini öğrenebilirsin");
+        player.PrintToCenter($" {CC.LR}[ZMTR] {CC.G} Seviye sisteminden ayrılmak istersen {CC.R}!seviyeayril {CC.G} ile ayrılabilirsin");
+        player.PrintToCenter($" {CC.LR}[ZMTR] {CC.G} Seviye sisteminin ayrıcalıkları hakkında bilgi almak için discorda katılmalısın ({CC.B}!dc{CC.G})");
+        player.PrintToCenter($" {CC.LR}[ZMTR] {CC.Gr} TEKRAR HOŞ GELDİN");
     }
 
     #endregion SeviyeOl
