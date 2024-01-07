655e7ace4338e98ae5e3e8d1008e17a48754d4f6
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-01-07 16:05:38 +0300

    disable disconnect thing

--- Events/EventPlayerDisconnect.cs
+++ Events/EventPlayerDisconnect.cs
@@ -12,33 +12,15 @@ public partial class JailbreakExtras
         {
             if (@event == null)
                 return HookResult.Continue;
-            if (ValidateCallerPlayer(@event.Userid, false) == false)
-            {
-                return HookResult.Continue;
-            }
-            ClearOnDisconnect(@event.Userid.SteamID, @event.Userid.UserId);
+            //if (ValidateCallerPlayer(@event.Userid, false) == false)
+            //{
+            //    return HookResult.Continue;
+            //}
+            //ClearOnDisconnect(@event.Userid.SteamID, @event.Userid.UserId);
             //if (@event?.Userid?.SteamID == LatestWCommandUser)
             //{
             //    CoinRemove();
             //}
-            return HookResult.Continue;
-        });
-        RegisterEventHandler<EventPlayerConnectFull>((@event, _) =>
-        {
-            if (@event == null)
-                return HookResult.Continue;
-            if (ValidateCallerPlayer(@event.Userid, false) == false)
-            {
-                return HookResult.Continue;
-            }
-            var player = @event.Userid;
-
-            AddOrUpdatePlayerToPlayerNameTable(player!.SteamID, player.PlayerName);
-            GetPlayerMarketData(player!.SteamID).Wait();
-            InsertAndGetTimeTrackingData(player.SteamID);
-            GetPGagData(player.SteamID);
-            InsertAndGetPlayerLevelData(player.SteamID, true);
-            CheckPlayerGroups(player);
             return HookResult.Continue;
         });
     }
--- Listeners/ListenersOnClientDisconnect.cs
+++ Listeners/ListenersOnClientDisconnect.cs
@@ -8,21 +8,21 @@ public partial class JailbreakExtras
     {
         RegisterListener<Listeners.OnClientDisconnect>(playerSlot =>
         {
-            uint finalSlot = (uint)playerSlot + 1;
-            CCSPlayerController player = new CCSPlayerController(NativeAPI.GetEntityFromIndex((int)finalSlot));
-            if (player == null || player.UserId < 0)
-                return;
-            if (player?.SteamID != null && player!.SteamID != 0)
-            {
-                if (player?.SteamID == LatestWCommandUser)
-                {
-                    CoinRemove();
-                }
-                Task.Run(async () =>
-                {
-                    await UpdatePlayerMarketData(player!.SteamID);
-                });
-            }
+            //uint finalSlot = (uint)playerSlot + 1;
+            //CCSPlayerController player = new CCSPlayerController(NativeAPI.GetEntityFromIndex((int)finalSlot));
+            //if (player == null || player.UserId < 0)
+            //    return;
+            //if (player?.SteamID != null && player!.SteamID != 0)
+            //{
+            //    if (player?.SteamID == LatestWCommandUser)
+            //    {
+            //        CoinRemove();
+            //    }
+            //    Task.Run(async () =>
+            //    {
+            //        await UpdatePlayerMarketData(player!.SteamID);
+            //    });
+            //}
         });
     }
 }
