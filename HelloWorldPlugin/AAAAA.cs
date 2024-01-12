5c2ea589b4c7b8a1d0bef26ecaadbc39ad4a303c
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-01-13 00:05:01 +0300

    aaa update

--- HelloWorldPlugin/AAAAA.cs
+++ HelloWorldPlugin/AAAAA.cs
@@ -20,34 +20,37 @@ public class AAAAA : BasePlugin
     {
         RegisterEventHandler((GameEventHandler<EventPlayerSpawn>)((@event, _) =>
         {
-            if (@event == null)
-                return HookResult.Continue;
+            if (@event == null) return HookResult.Continue;
+            if (@event.Userid == null) return HookResult.Continue;
+            if (@event.Userid.IsValid == false) return HookResult.Continue;
+            if (@event.Userid.UserId < 0) return HookResult.Continue;
+            if (@event.Userid.SteamID < 0) return HookResult.Continue;
             var player = @event.Userid;
-            if (player != null && player.IsValid && player.PlayerPawn.IsValid)
+            if (IsValid(player))
             {
-                if (IsValid(player))
+                if ((CsTeam)player.TeamNum == CsTeam.CounterTerrorist)
                 {
-                    if ((CsTeam)player.TeamNum == CsTeam.CounterTerrorist)
-                    {
-                        if (IsValid(player))
-                        {
-                            player.GiveNamedItem("item_assaultsuit");
-                            player.GiveNamedItem("weapon_deagle");
-                            player.GiveNamedItem("weapon_m4a1");
-                        }
-                    }
                     if (IsValid(player))
                     {
-                        SetColour(@event.Userid, DefaultColor);
+                        player.GiveNamedItem("item_assaultsuit");
+                        player.GiveNamedItem("weapon_deagle");
+                        player.GiveNamedItem("weapon_m4a1");
                     }
+                }
+                if (IsValid(player))
+                {
+                    SetColour(@event.Userid, DefaultColor);
                 }
             }
             return HookResult.Continue;
         }));
         RegisterEventHandler<EventPlayerDeath>((@event, info) =>
         {
-            if (@event == null)
-                return HookResult.Continue;
+            if (@event == null) return HookResult.Continue;
+            if (@event.Userid == null) return HookResult.Continue;
+            if (@event.Userid.IsValid == false) return HookResult.Continue;
+            if (@event.Userid.UserId < 0) return HookResult.Continue;
+            if (@event.Userid.SteamID < 0) return HookResult.Continue;
             if (IsValid(@event.Userid))
             {
                 SetColour(@event.Userid, Color.FromArgb(0, 0, 0, 0));
