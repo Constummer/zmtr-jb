fbfee2d151b76fc430b2704464c4f0b315dcdd79
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-01-14 17:48:36 +0300

    add kafa ac

---
+++ Commands/KafaAc.cs
@@ -0,0 +1,37 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+
+namespace JailbreakExtras;
+
+public partial class JailbreakExtras
+{
+    #region RR
+
+    [ConsoleCommand("kafakapat")]
+    [ConsoleCommand("kafakapa")]
+    [ConsoleCommand("hskapa")]
+    [ConsoleCommand("hskapat")]
+    public void KafaKapat(CCSPlayerController? player, CommandInfo info)
+    {
+        if (ValidateCallerPlayer(player) == false)
+        {
+            return;
+        }
+        Server.ExecuteCommand($"mp_damage_headshot_only 0");
+    }
+
+    [ConsoleCommand("kafaac")]
+    [ConsoleCommand("hsac")]
+    public void KafaAc(CCSPlayerController? player, CommandInfo info)
+    {
+        if (ValidateCallerPlayer(player) == false)
+        {
+            return;
+        }
+        Server.ExecuteCommand($"mp_damage_headshot_only 1");
+    }
+
+    #endregion RR
+}
