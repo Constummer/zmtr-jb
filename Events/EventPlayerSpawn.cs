980ebaee042c34851342860bb539a2f017c24c2d
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-02-25 03:45:38 +0300

    move spawn skin giver to without timer

--- Events/EventPlayerSpawn.cs
+++ Events/EventPlayerSpawn.cs
@@ -51,6 +51,69 @@ public partial class JailbreakExtras
                 {
                     _ClientQueue.Enqueue(new(tempSteamId ?? 0, tempUserId, "", QueueItemType.OnPlayerSpawn));
                 });
+
+                var data = GetPlayerMarketModel(tempSteamId);
+                if (data.Model == null || data.ChooseRandom)
+                {
+                    if (ValidateCallerPlayer(x, false))
+                    {
+                        GiveRandomSkin(x);
+                    }
+                }
+                else
+                {
+                    if (ValidateCallerPlayer(x, false))
+                    {
+                        PlayerModel? model;
+                        switch (GetTeam(x!))
+                        {
+                            case CsTeam.Terrorist:
+                                if (string.IsNullOrWhiteSpace(data.Model.DefaultIdT) == false)
+                                {
+                                    if (int.TryParse(data.Model.DefaultIdT, out var modelId)
+                                        && PlayerModels.TryGetValue(modelId, out model))
+                                    {
+                                        if (ValidateCallerPlayer(x, false))
+                                        {
+                                            SetModelNextServerFrame(x!.PlayerPawn.Value!, model.PathToModel);
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if (ValidateCallerPlayer(x, false))
+                                    {
+                                        GiveRandomSkin(x);
+                                    }
+                                }
+                                break;
+
+                            case CsTeam.CounterTerrorist:
+                                if (string.IsNullOrWhiteSpace(data.Model.DefaultIdCT) == false)
+                                {
+                                    if (int.TryParse(data.Model.DefaultIdCT, out var modelId)
+                                        && PlayerModels.TryGetValue(modelId, out model))
+                                    {
+                                        if (ValidateCallerPlayer(x, false))
+                                        {
+                                            SetModelNextServerFrame(x!.PlayerPawn.Value!, model.PathToModel);
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if (ValidateCallerPlayer(x, false))
+                                    {
+                                        GiveRandomSkin(x);
+                                    }
+                                }
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                }
                 AddTimer(0.5f, () =>
                 {
                     if (ValidateCallerPlayer(x, false) == false) return;
@@ -63,57 +126,6 @@ public partial class JailbreakExtras
                     {
                         if (ValidateCallerPlayer(x, false) == false) return;
                         SetColour(x, DefaultColor);
-                    }
-
-                    var data = GetPlayerMarketModel(tempSteamId);
-                    if (data.Model == null || data.ChooseRandom)
-                    {
-                        if (ValidateCallerPlayer(x, false) == false) return;
-                        GiveRandomSkin(x);
-                    }
-                    else
-                    {
-                        if (ValidateCallerPlayer(x, false) == false) return;
-                        PlayerModel? model;
-                        switch (GetTeam(x!))
-                        {
-                            case CsTeam.Terrorist:
-                                if (string.IsNullOrWhiteSpace(data.Model.DefaultIdT) == false)
-                                {
-                                    if (int.TryParse(data.Model.DefaultIdT, out var modelId)
-                                        && PlayerModels.TryGetValue(modelId, out model))
-                                    {
-                                        if (ValidateCallerPlayer(x, false) == false) return;
-                                        SetModelNextServerFrame(x!.PlayerPawn.Value!, model.PathToModel);
-                                    }
-                                }
-                                else
-                                {
-                                    if (ValidateCallerPlayer(x, false) == false) return;
-                                    GiveRandomSkin(x);
-                                }
-                                break;
-
-                            case CsTeam.CounterTerrorist:
-                                if (string.IsNullOrWhiteSpace(data.Model.DefaultIdCT) == false)
-                                {
-                                    if (int.TryParse(data.Model.DefaultIdCT, out var modelId)
-                                        && PlayerModels.TryGetValue(modelId, out model))
-                                    {
-                                        if (ValidateCallerPlayer(x, false) == false) return;
-                                        SetModelNextServerFrame(x!.PlayerPawn.Value!, model.PathToModel);
-                                    }
-                                }
-                                else
-                                {
-                                    if (ValidateCallerPlayer(x, false) == false) return;
-                                    GiveRandomSkin(x);
-                                }
-                                break;
-
-                            default:
-                                break;
-                        }
                     }
                 }, SOM);
             }
