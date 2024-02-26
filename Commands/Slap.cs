bd0cf99869a68345ad244aa6882322ce985d168b
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-02-26 22:58:04 +0300

    tryadd uberslap

--- Commands/Slap.cs
+++ Commands/Slap.cs
@@ -8,6 +8,8 @@ namespace JailbreakExtras;
 
 public partial class JailbreakExtras
 {
+    public CounterStrikeSharp.API.Modules.Timers.Timer? UberSlapTimer { get; set; } = null;
+
     #region Slap
 
     [ConsoleCommand("slap")]
@@ -42,6 +44,67 @@ public partial class JailbreakExtras
         {
             Server.PrintToChatAll($"{AdliAdmin(player.PlayerName)} {CC.G}{target} {CC.W}hedefine {CC.B}{damage} {CC.W}slapledi.");
         }
+    }
+
+    [ConsoleCommand("uberslap")]
+    [CommandHelper(1, "<oyuncu ismi,@t,@ct,@all,@me> [hasar]")]
+    public void OnUberSlapCommand(CCSPlayerController? player, CommandInfo info)
+    {
+        if (OnCommandValidater(player, true, "@css/seviye10") == false)
+        {
+            return;
+        }
+        var target = info.ArgString.GetArg(0);
+        int damage = 0;
+
+        if (info.ArgCount >= 2)
+        {
+            int.TryParse(info.ArgString.GetArg(1), out damage);
+        }
+        var targetArgument = GetTargetArgument(target);
+        var players = GetPlayers()
+               .Where(x => x.PawnIsAlive
+                          && GetTargetAction(x, target, player))
+               .ToList();
+        if (players.Count > 0)
+        {
+            players.ForEach(x =>
+            {
+                if (ValidateCallerPlayer(x, false) == false) return;
+                if (targetArgument == TargetForArgument.SingleUser)
+                {
+                    Server.PrintToChatAll($"{AdliAdmin(player.PlayerName)} {CC.B}{x.PlayerName} {CC.W}adlı oyuncuya {CC.B}{damage} {CC.W}uberslapledi.");
+                }
+            });
+            UberSlapTimer?.Kill();
+
+            UberSlapTimer = AddTimer(0.1f, () =>
+              {
+                  players.ForEach(x =>
+                  {
+                      if (ValidateCallerPlayer(x, false) == false) return;
+                      PerformSlap(x!.Pawn.Value!, damage);
+                  });
+              }, Full);
+            if (targetArgument != TargetForArgument.SingleUser)
+            {
+                Server.PrintToChatAll($"{AdliAdmin(player.PlayerName)} {CC.G}{target} {CC.W}hedefine {CC.B}{damage} {CC.W}slapledi.");
+            }
+        }
+    }
+
+    [ConsoleCommand("uberslapkapa")]
+    [ConsoleCommand("uberslapkapat")]
+    [ConsoleCommand("uberslapiptal")]
+    [CommandHelper(1, "<oyuncu ismi,@t,@ct,@all,@me> ")]
+    public void OnUberSlapKapaCommand(CCSPlayerController? player, CommandInfo info)
+    {
+        if (OnCommandValidater(player, true, "@css/seviye10") == false)
+        {
+            return;
+        }
+        UberSlapTimer?.Kill();
+        Server.PrintToChatAll($"{AdliAdmin(player.PlayerName)} {CC.W}uberslapi kapadı.");
     }
 
     private static void PerformSlap(CBasePlayerPawn pawn, int damage = 0)
