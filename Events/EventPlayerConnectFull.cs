f64c66de7d822fb04560744f59c01fd2e939ec1b
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-04-16 01:41:49 +0300

    add new event handler for initial tests

--- Events/EventPlayerConnectFull.cs
+++ Events/EventPlayerConnectFull.cs
@@ -1,3 +1,4 @@
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 
 namespace JailbreakExtras;
@@ -6,7 +7,43 @@ public partial class JailbreakExtras
 {
     private void EventPlayerConnectFull()
     {
-        //disabled, was causing crash
+        RegisterEventHandler<EventPlayerChat>((@event, info) =>
+        {
+            //Server.PrintToChatAll($"{@event.Teamonly}");
+            //Server.PrintToChatAll($"{@event.Text}");
+            //Server.PrintToChatAll($"{@event.Userid}");
+            //var p = Utilities.GetPlayerFromUserid(@event.Userid);
+            //if (p != null)
+            //{
+            //    if (ValidateCallerPlayer(p, false) == false)
+            //    {
+            //        return HookResult.Continue;
+            //    }
+            //    Server.PrintToChatAll(p.PlayerName);
+            //    return HookResult.Continue;
+            //}
+            /*
+             False
+            tamam
+            3
+            smartest indian
+            False
+            tamam
+            3
+            smartest indian
+            [KOMUTÇU] smartest indian : tamam
+            False
+            !gag @all
+            1
+            Constummer
+            False
+            !gag @all
+            1
+            Constummer
+            *ÖLÜ* [Dev] Constummer : !gag @all
+            */
+            return HookResult.Continue;
+        });
         RegisterEventHandler<EventPlayerConnectFull>((@event, _) =>
         {
             //if (@event == null) return HookResult.Continue;
