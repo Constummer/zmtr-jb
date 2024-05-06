61597ac1dfb7c586c7d049a500212815bee139ce
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-05-06 12:32:22 +0300

    add findSinglePlayer as test

--- Commands/Level/TagKaldir.cs
+++ Commands/Level/TagKaldir.cs
@@ -3,6 +3,7 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Commands.Targeting;
 
 namespace JailbreakExtras;
 
@@ -36,6 +37,42 @@ public partial class JailbreakExtras
                 Utilities.SetStateChanged(player, "CBasePlayerController", "m_iszPlayerName");
             }, SOM);
             player.PrintToChat($"{Prefix}{CC.B} !tagac{CC.W} ile tagini tekrardan acabilirsin.");
+        }
+    }
+
+    [ConsoleCommand("taginikaldir")]
+    [ConsoleCommand("taginikapat")]
+    [ConsoleCommand("taginisil")]
+    public void TaginiKaldir(CCSPlayerController? player, CommandInfo info)
+    {
+        if (!AdminManager.PlayerHasPermissions(player, "@css/root"))
+        {
+            player.PrintToChat(NotEnoughPermission);
+            return;
+        }
+        if (ValidateCallerPlayer(player, false) == false)
+        {
+            return;
+        }
+        var target = info.ArgString.GetArgSkip(0);
+
+        if (FindSinglePlayer(player, target, out var x) == false || ValidateCallerPlayer(x, false) == false)
+        {
+            return;
+        }
+        LogManagerCommand(player.SteamID, info.GetCommandString);
+        if (LevelTagDisabledPlayers.Any(y => y == x.SteamID) == false)
+        {
+            LevelTagDisabledPlayers.Add(x.SteamID);
+            UpdatePlayerLevelTagDisableData(x.SteamID, true);
+            x.Clan = null;
+            AddTimer(0.2f, () =>
+            {
+                if (ValidateCallerPlayer(x, false) == false) return;
+                Utilities.SetStateChanged(x, "CCSPlayerController", "m_szClan");
+                Utilities.SetStateChanged(x, "CBasePlayerController", "m_iszPlayerName");
+            }, SOM);
+            x.PrintToChat($"{AdliAdmin(player.PlayerName)}{CC.B} tagini {CC.W} kapatti.");
         }
     }
 
---
+++ Lib/Helpers/FindSinglePlayer.cs
@@ -0,0 +1,25 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace JailbreakExtras;
+
+public partial class JailbreakExtras
+{
+    private bool FindSinglePlayer(CCSPlayerController player, string target, out CCSPlayerController? result)
+    {
+        result = null;
+        var players = GetPlayers()
+           .Where(x => GetTargetAction(x, target, player));
+        if (players.Count() == 0)
+        {
+            player.PrintToChat($"{Prefix} {CC.W}Eşleşen oyuncu bulunamadı!");
+            return false;
+        }
+        if (players.Count() != 1)
+        {
+            player.PrintToChat($"{Prefix} {CC.W}Birden fazla oyuncu bulundu.");
+            return false;
+        }
+        result = players.FirstOrDefault();
+        return true;
+    }
+}
