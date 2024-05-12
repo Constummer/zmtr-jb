160ab2e9982a45f80c1558c26f003fc6c90cf027
Onurcan ertekin <ertekinonurcan@gmail.com>
2024-05-13 00:32:09 +0300

    fix gel and add rgel

---
+++ Commands/RGel.cs
@@ -0,0 +1,68 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace JailbreakExtras;
+
+public partial class JailbreakExtras
+{
+    #region Disarm
+
+    [ConsoleCommand("rgel")]
+    public void RGel(CCSPlayerController? player, CommandInfo info)
+    {
+        if (ValidateCallerPlayer(player, false) == false)
+        {
+            return;
+        }
+        if (!AdminManager.PlayerHasPermissions(player, "@css/premium"))
+        {
+            player.PrintToChat(NotEnoughPermission);
+            return;
+        }
+        var target = info.ArgString.GetArgSkip(0);
+        var targetArgument = GetTargetArgument(target);
+        LogManagerCommand(player.SteamID, info.GetCommandString);
+
+        GetPlayers()
+                   .Where(x => x.PawnIsAlive == false && GetTargetAction(x, target, player))
+                   .ToList()
+                   .ForEach(x =>
+                   {
+                       if ((targetArgument & TargetForArgument.SingleUser) == targetArgument)
+                       {
+                           Server.PrintToChatAll($"{AdliAdmin(player.PlayerName)} {CC.G}{x.PlayerName} {CC.W}adlı oyuncuyu{CC.B} revledi{CC.W}.");
+                       }
+                       CustomRespawn(x);
+                   });
+        if ((targetArgument & TargetForArgument.SingleUser) != targetArgument)
+        {
+            Server.PrintToChatAll($"{AdliAdmin(player.PlayerName)} {CC.G}{target} {CC.W}hedefini {CC.B}revledi");
+        }
+
+        GetPlayers()
+                   .Where(x => x.PawnIsAlive && GetTargetAction(x, target, player))
+                   .ToList()
+                   .ForEach(x =>
+                   {
+                       if (x.SteamID != player.SteamID)
+                       {
+                           if ((targetArgument & TargetForArgument.SingleUser) == targetArgument)
+                           {
+                               Server.PrintToChatAll($"{AdliAdmin(player.PlayerName)} {CC.G}{x.PlayerName} {CC.W}adlı oyuncuyu{CC.B} yanına ışınladı{CC.W}.");
+                           }
+                           var playerAbs = player.PawnIsAlive == false ? player.Pawn.Value.AbsOrigin : player.PlayerPawn.Value.AbsOrigin;
+                           x.PlayerPawn.Value.Teleport(new Vector(playerAbs.X, playerAbs.Y + 1, playerAbs.Z), x.PlayerPawn.Value.EyeAngles, VEC_ZERO);
+                       }
+                   });
+        if ((targetArgument & TargetForArgument.SingleUser) != targetArgument)
+        {
+            Server.PrintToChatAll($"{AdliAdmin(player.PlayerName)} {CC.G}{target} {CC.W}hedefini {CC.B}yanına ışınladı");
+        }
+    }
+
+    #endregion Disarm
+}
